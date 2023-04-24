a840605bf7c675fd84e6526d7e54cc1ccc106505
javanyam <elyje5907@gmail.com>
2023-04-24 22:56:21 +0900

    create

---
+++ ChuLoop/ChuLoop/ChuLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChuLoopApp.swift
+//  ChuLoop
+//
+//  Created by 예띤 on 2023/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ChuLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
